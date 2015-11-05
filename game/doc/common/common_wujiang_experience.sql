a4266beb01d1180e712b18e0570175f76cbd4b48
孙永清 <s_yongqin@sina.com>
2015-11-05 22:32:22 +0800

    新建common_wujiang_experience.sql

---
+++ game/doc/common/common_wujiang_experience.sql
@@ -0,0 +1,241 @@
+DROP TABLE IF EXISTS `common_wujiang_experience`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `common_wujiang_experience` (
+  `level` int(3) NOT NULL COMMENT 'level',
+  `experience` int(11) NOT NULL COMMENT '经验',
+  PRIMARY KEY (`level`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='武将升级经验表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : 120.27.49.196
+Source Server Version : 50173
+Source Host           : 120.27.49.196:3306
+Source Database       : test
+
+Target Server Type    : MYSQL
+Target Server Version : 50173
+File Encoding         : 65001
+
+Date: 2015-11-05 17:31:56
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `common_wujiang_experience`
+-- ----------------------------
+DROP TABLE IF EXISTS `common_wujiang_experience`;
+CREATE TABLE `common_wujiang_experience` (
+  `level` int(3) NOT NULL COMMENT 'level',
+  `experience` int(11) NOT NULL COMMENT '????',
+  PRIMARY KEY (`level`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='?佫?????????';
+
+-- ----------------------------
+-- Records of common_wujiang_experience
+-- ----------------------------
+INSERT INTO `common_wujiang_experience` VALUES ('1', '0');
+INSERT INTO `common_wujiang_experience` VALUES ('2', '11');
+INSERT INTO `common_wujiang_experience` VALUES ('3', '24');
+INSERT INTO `common_wujiang_experience` VALUES ('4', '39');
+INSERT INTO `common_wujiang_experience` VALUES ('5', '56');
+INSERT INTO `common_wujiang_experience` VALUES ('6', '75');
+INSERT INTO `common_wujiang_experience` VALUES ('7', '96');
+INSERT INTO `common_wujiang_experience` VALUES ('8', '119');
+INSERT INTO `common_wujiang_experience` VALUES ('9', '144');
+INSERT INTO `common_wujiang_experience` VALUES ('10', '171');
+INSERT INTO `common_wujiang_experience` VALUES ('11', '200');
+INSERT INTO `common_wujiang_experience` VALUES ('12', '231');
+INSERT INTO `common_wujiang_experience` VALUES ('13', '264');
+INSERT INTO `common_wujiang_experience` VALUES ('14', '299');
+INSERT INTO `common_wujiang_experience` VALUES ('15', '336');
+INSERT INTO `common_wujiang_experience` VALUES ('16', '375');
+INSERT INTO `common_wujiang_experience` VALUES ('17', '416');
+INSERT INTO `common_wujiang_experience` VALUES ('18', '459');
+INSERT INTO `common_wujiang_experience` VALUES ('19', '504');
+INSERT INTO `common_wujiang_experience` VALUES ('20', '551');
+INSERT INTO `common_wujiang_experience` VALUES ('21', '600');
+INSERT INTO `common_wujiang_experience` VALUES ('22', '651');
+INSERT INTO `common_wujiang_experience` VALUES ('23', '704');
+INSERT INTO `common_wujiang_experience` VALUES ('24', '759');
+INSERT INTO `common_wujiang_experience` VALUES ('25', '816');
+INSERT INTO `common_wujiang_experience` VALUES ('26', '875');
+INSERT INTO `common_wujiang_experience` VALUES ('27', '936');
+INSERT INTO `common_wujiang_experience` VALUES ('28', '999');
+INSERT INTO `common_wujiang_experience` VALUES ('29', '1064');
+INSERT INTO `common_wujiang_experience` VALUES ('30', '1131');
+INSERT INTO `common_wujiang_experience` VALUES ('31', '1200');
+INSERT INTO `common_wujiang_experience` VALUES ('32', '1271');
+INSERT INTO `common_wujiang_experience` VALUES ('33', '1344');
+INSERT INTO `common_wujiang_experience` VALUES ('34', '1419');
+INSERT INTO `common_wujiang_experience` VALUES ('35', '1496');
+INSERT INTO `common_wujiang_experience` VALUES ('36', '1575');
+INSERT INTO `common_wujiang_experience` VALUES ('37', '1656');
+INSERT INTO `common_wujiang_experience` VALUES ('38', '1739');
+INSERT INTO `common_wujiang_experience` VALUES ('39', '1824');
+INSERT INTO `common_wujiang_experience` VALUES ('40', '1911');
+INSERT INTO `common_wujiang_experience` VALUES ('41', '2000');
+INSERT INTO `common_wujiang_experience` VALUES ('42', '2091');
+INSERT INTO `common_wujiang_experience` VALUES ('43', '2184');
+INSERT INTO `common_wujiang_experience` VALUES ('44', '2279');
+INSERT INTO `common_wujiang_experience` VALUES ('45', '2376');
+INSERT INTO `common_wujiang_experience` VALUES ('46', '2475');
+INSERT INTO `common_wujiang_experience` VALUES ('47', '2576');
+INSERT INTO `common_wujiang_experience` VALUES ('48', '2679');
+INSERT INTO `common_wujiang_experience` VALUES ('49', '2784');
+INSERT INTO `common_wujiang_experience` VALUES ('50', '2891');
+INSERT INTO `common_wujiang_experience` VALUES ('51', '3000');
+INSERT INTO `common_wujiang_experience` VALUES ('52', '3111');
+INSERT INTO `common_wujiang_experience` VALUES ('53', '3224');
+INSERT INTO `common_wujiang_experience` VALUES ('54', '3339');
+INSERT INTO `common_wujiang_experience` VALUES ('55', '3456');
+INSERT INTO `common_wujiang_experience` VALUES ('56', '3575');
+INSERT INTO `common_wujiang_experience` VALUES ('57', '3696');
+INSERT INTO `common_wujiang_experience` VALUES ('58', '3819');
+INSERT INTO `common_wujiang_experience` VALUES ('59', '3944');
+INSERT INTO `common_wujiang_experience` VALUES ('60', '4071');
+INSERT INTO `common_wujiang_experience` VALUES ('61', '4200');
+INSERT INTO `common_wujiang_experience` VALUES ('62', '4331');
+INSERT INTO `common_wujiang_experience` VALUES ('63', '4464');
+INSERT INTO `common_wujiang_experience` VALUES ('64', '4599');
+INSERT INTO `common_wujiang_experience` VALUES ('65', '4736');
+INSERT INTO `common_wujiang_experience` VALUES ('66', '4875');
+INSERT INTO `common_wujiang_experience` VALUES ('67', '5016');
+INSERT INTO `common_wujiang_experience` VALUES ('68', '5159');
+INSERT INTO `common_wujiang_experience` VALUES ('69', '5304');
+INSERT INTO `common_wujiang_experience` VALUES ('70', '5451');
+INSERT INTO `common_wujiang_experience` VALUES ('71', '5600');
+INSERT INTO `common_wujiang_experience` VALUES ('72', '5751');
+INSERT INTO `common_wujiang_experience` VALUES ('73', '5904');
+INSERT INTO `common_wujiang_experience` VALUES ('74', '6059');
+INSERT INTO `common_wujiang_experience` VALUES ('75', '6216');
+INSERT INTO `common_wujiang_experience` VALUES ('76', '6375');
+INSERT INTO `common_wujiang_experience` VALUES ('77', '6536');
+INSERT INTO `common_wujiang_experience` VALUES ('78', '6699');
+INSERT INTO `common_wujiang_experience` VALUES ('79', '6864');
+INSERT INTO `common_wujiang_experience` VALUES ('80', '7031');
+INSERT INTO `common_wujiang_experience` VALUES ('81', '7200');
+INSERT INTO `common_wujiang_experience` VALUES ('82', '7371');
+INSERT INTO `common_wujiang_experience` VALUES ('83', '7544');
+INSERT INTO `common_wujiang_experience` VALUES ('84', '7719');
+INSERT INTO `common_wujiang_experience` VALUES ('85', '7896');
+INSERT INTO `common_wujiang_experience` VALUES ('86', '8075');
+INSERT INTO `common_wujiang_experience` VALUES ('87', '8256');
+INSERT INTO `common_wujiang_experience` VALUES ('88', '8439');
+INSERT INTO `common_wujiang_experience` VALUES ('89', '8624');
+INSERT INTO `common_wujiang_experience` VALUES ('90', '8811');
+INSERT INTO `common_wujiang_experience` VALUES ('91', '9000');
+INSERT INTO `common_wujiang_experience` VALUES ('92', '9191');
+INSERT INTO `common_wujiang_experience` VALUES ('93', '9384');
+INSERT INTO `common_wujiang_experience` VALUES ('94', '9579');
+INSERT INTO `common_wujiang_experience` VALUES ('95', '9776');
+INSERT INTO `common_wujiang_experience` VALUES ('96', '9975');
+INSERT INTO `common_wujiang_experience` VALUES ('97', '10176');
+INSERT INTO `common_wujiang_experience` VALUES ('98', '10379');
+INSERT INTO `common_wujiang_experience` VALUES ('99', '10584');
+INSERT INTO `common_wujiang_experience` VALUES ('100', '10791');
+INSERT INTO `common_wujiang_experience` VALUES ('101', '11000');
+INSERT INTO `common_wujiang_experience` VALUES ('102', '11211');
+INSERT INTO `common_wujiang_experience` VALUES ('103', '11424');
+INSERT INTO `common_wujiang_experience` VALUES ('104', '11639');
+INSERT INTO `common_wujiang_experience` VALUES ('105', '11856');
+INSERT INTO `common_wujiang_experience` VALUES ('106', '12075');
+INSERT INTO `common_wujiang_experience` VALUES ('107', '12296');
+INSERT INTO `common_wujiang_experience` VALUES ('108', '12519');
+INSERT INTO `common_wujiang_experience` VALUES ('109', '12744');
+INSERT INTO `common_wujiang_experience` VALUES ('110', '12971');
+INSERT INTO `common_wujiang_experience` VALUES ('111', '13200');
+INSERT INTO `common_wujiang_experience` VALUES ('112', '13431');
+INSERT INTO `common_wujiang_experience` VALUES ('113', '13664');
+INSERT INTO `common_wujiang_experience` VALUES ('114', '13899');
+INSERT INTO `common_wujiang_experience` VALUES ('115', '14136');
+INSERT INTO `common_wujiang_experience` VALUES ('116', '14375');
+INSERT INTO `common_wujiang_experience` VALUES ('117', '14616');
+INSERT INTO `common_wujiang_experience` VALUES ('118', '14859');
+INSERT INTO `common_wujiang_experience` VALUES ('119', '15104');
+INSERT INTO `common_wujiang_experience` VALUES ('120', '15351');
+INSERT INTO `common_wujiang_experience` VALUES ('121', '15600');
+INSERT INTO `common_wujiang_experience` VALUES ('122', '15851');
+INSERT INTO `common_wujiang_experience` VALUES ('123', '16104');
+INSERT INTO `common_wujiang_experience` VALUES ('124', '16359');
+INSERT INTO `common_wujiang_experience` VALUES ('125', '16616');
+INSERT INTO `common_wujiang_experience` VALUES ('126', '16875');
+INSERT INTO `common_wujiang_experience` VALUES ('127', '17136');
+INSERT INTO `common_wujiang_experience` VALUES ('128', '17399');
+INSERT INTO `common_wujiang_experience` VALUES ('129', '17664');
+INSERT INTO `common_wujiang_experience` VALUES ('130', '17931');
+INSERT INTO `common_wujiang_experience` VALUES ('131', '18200');
+INSERT INTO `common_wujiang_experience` VALUES ('132', '18471');
+INSERT INTO `common_wujiang_experience` VALUES ('133', '18744');
+INSERT INTO `common_wujiang_experience` VALUES ('134', '19019');
+INSERT INTO `common_wujiang_experience` VALUES ('135', '19296');
+INSERT INTO `common_wujiang_experience` VALUES ('136', '19575');
+INSERT INTO `common_wujiang_experience` VALUES ('137', '19856');
+INSERT INTO `common_wujiang_experience` VALUES ('138', '20139');
+INSERT INTO `common_wujiang_experience` VALUES ('139', '20424');
+INSERT INTO `common_wujiang_experience` VALUES ('140', '20711');
+INSERT INTO `common_wujiang_experience` VALUES ('141', '21000');
+INSERT INTO `common_wujiang_experience` VALUES ('142', '21291');
+INSERT INTO `common_wujiang_experience` VALUES ('143', '21584');
+INSERT INTO `common_wujiang_experience` VALUES ('144', '21879');
+INSERT INTO `common_wujiang_experience` VALUES ('145', '22176');
+INSERT INTO `common_wujiang_experience` VALUES ('146', '22475');
+INSERT INTO `common_wujiang_experience` VALUES ('147', '22776');
+INSERT INTO `common_wujiang_experience` VALUES ('148', '23079');
+INSERT INTO `common_wujiang_experience` VALUES ('149', '23384');
+INSERT INTO `common_wujiang_experience` VALUES ('150', '23691');
+INSERT INTO `common_wujiang_experience` VALUES ('151', '24000');
+INSERT INTO `common_wujiang_experience` VALUES ('152', '24311');
+INSERT INTO `common_wujiang_experience` VALUES ('153', '24624');
+INSERT INTO `common_wujiang_experience` VALUES ('154', '24939');
+INSERT INTO `common_wujiang_experience` VALUES ('155', '25256');
+INSERT INTO `common_wujiang_experience` VALUES ('156', '25575');
+INSERT INTO `common_wujiang_experience` VALUES ('157', '25896');
+INSERT INTO `common_wujiang_experience` VALUES ('158', '26219');
+INSERT INTO `common_wujiang_experience` VALUES ('159', '26544');
+INSERT INTO `common_wujiang_experience` VALUES ('160', '26871');
+INSERT INTO `common_wujiang_experience` VALUES ('161', '27200');
+INSERT INTO `common_wujiang_experience` VALUES ('162', '27531');
+INSERT INTO `common_wujiang_experience` VALUES ('163', '27864');
+INSERT INTO `common_wujiang_experience` VALUES ('164', '28199');
+INSERT INTO `common_wujiang_experience` VALUES ('165', '28536');
+INSERT INTO `common_wujiang_experience` VALUES ('166', '28875');
+INSERT INTO `common_wujiang_experience` VALUES ('167', '29216');
+INSERT INTO `common_wujiang_experience` VALUES ('168', '29559');
+INSERT INTO `common_wujiang_experience` VALUES ('169', '29904');
+INSERT INTO `common_wujiang_experience` VALUES ('170', '30251');
+INSERT INTO `common_wujiang_experience` VALUES ('171', '30600');
+INSERT INTO `common_wujiang_experience` VALUES ('172', '30951');
+INSERT INTO `common_wujiang_experience` VALUES ('173', '31304');
+INSERT INTO `common_wujiang_experience` VALUES ('174', '31659');
+INSERT INTO `common_wujiang_experience` VALUES ('175', '32016');
+INSERT INTO `common_wujiang_experience` VALUES ('176', '32375');
+INSERT INTO `common_wujiang_experience` VALUES ('177', '32736');
+INSERT INTO `common_wujiang_experience` VALUES ('178', '33099');
+INSERT INTO `common_wujiang_experience` VALUES ('179', '33464');
+INSERT INTO `common_wujiang_experience` VALUES ('180', '33831');
+INSERT INTO `common_wujiang_experience` VALUES ('181', '34200');
+INSERT INTO `common_wujiang_experience` VALUES ('182', '34571');
+INSERT INTO `common_wujiang_experience` VALUES ('183', '34944');
+INSERT INTO `common_wujiang_experience` VALUES ('184', '35319');
+INSERT INTO `common_wujiang_experience` VALUES ('185', '35696');
+INSERT INTO `common_wujiang_experience` VALUES ('186', '36075');
+INSERT INTO `common_wujiang_experience` VALUES ('187', '36456');
+INSERT INTO `common_wujiang_experience` VALUES ('188', '36839');
+INSERT INTO `common_wujiang_experience` VALUES ('189', '37224');
+INSERT INTO `common_wujiang_experience` VALUES ('190', '37611');
+INSERT INTO `common_wujiang_experience` VALUES ('191', '38000');
+INSERT INTO `common_wujiang_experience` VALUES ('192', '38391');
+INSERT INTO `common_wujiang_experience` VALUES ('193', '38784');
+INSERT INTO `common_wujiang_experience` VALUES ('194', '39179');
+INSERT INTO `common_wujiang_experience` VALUES ('195', '39576');
+INSERT INTO `common_wujiang_experience` VALUES ('196', '39975');
+INSERT INTO `common_wujiang_experience` VALUES ('197', '40376');
+INSERT INTO `common_wujiang_experience` VALUES ('198', '40779');
+INSERT INTO `common_wujiang_experience` VALUES ('199', '41184');
+INSERT INTO `common_wujiang_experience` VALUES ('200', '41591');
