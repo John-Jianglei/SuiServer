3c64686dbf5c1b46eea5cc14dfee5c39e578b2a6
john <s_yongqing@163.com>
2015-12-08 11:26:59 +0800

    debug RandomUtil

--- game/src/main/java/com/shinian/util/RandomUtil.java
+++ game/src/main/java/com/shinian/util/RandomUtil.java
@@ -13,25 +13,31 @@ public class RandomUtil {
 
 	public static int[] random(int min, int max, int n)
 	{
-	    if (n > (max - min + 1) || max < min) {  
-	           return null;  
-	       }  
-	    int[] result = new int[n];  
-	    int count = 0;  
-	    while(count < n) {  
-	        int num = (int) (Math.random() * (max - min)) + min;  
-	        boolean flag = true;  
-	        for (int j = 0; j < n; j++) {  
-	            if(num == result[j]){  
-	                flag = false;  
-	                break;  
-	            }  
-	        }  
-	        if(flag){  
-	            result[count] = num;  
-	            count++;  
-	        }  
-	    }  
-	    return result;
+		if (max < min){
+			int t = max;
+			max = min;
+			min = t;
+		}
+		
+		if (n > (max - min + 1)) n = max - min + 1;
+		 
+	
+		int[] result = new int[n];  
+	    	int count = 0;  
+	    	while(count < n) {  
+	        	int num = (int) (Math.random() * (max - min)) + min;  
+	        	boolean flag = true;  
+	        	for (int j = 0; j < n; j++) {  
+	            		if(num == result[j]){  
+	        	        	flag = false;  
+	        	        	break;  
+	            		}  
+			}  
+			if(flag){  
+				result[count] = num;  
+				count++;  
+			}  
+		}  
+		return result;
 	}
 }
